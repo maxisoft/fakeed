b3791ca38d94c9f9d17d099bfa1b371a55a6ee00
maxisoft <maxisoft.eu@gmail.com>
2015-04-12 15:22:28 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Command
-from unittest import TextTestRunner, TestLoader
-import os
-import os.path
+from distutils.core import setup
 
-
-class TestCommand(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        retval = os.system('python -m test')
-        if retval != 0:
-            raise Exception('tests failed')
 
 
 setup(
-    name='tornado-proxy',
+    name='fakeed',
     version='0.1',
-    description='Simple asynchronous HTTP proxy',
-    url='http://senko.net/en/',
-    author='Senko Rasic',
-    author_email='senko.rasic@dobarkod.hr',
-    cmdclass={
-        'test': TestCommand
-    },
-    install_requires=['tornado'],
-    packages=['tornado_proxy'],
+    description='Fake torrent tracker upload statistics',
+    url='https://github.com/maxisoft/fakeed',
+    author='maxisoft',
+    author_email='maxisoft@youpy.tk',
+    install_requires=['tornado', 'pyyaml'],
+    packages=['fakeed'],
 )
